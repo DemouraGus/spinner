4744ed39ab4be3e28b6f71226fd118969644251f
DemouraGus <demoura.gus@gmail.com>
2024-11-19 09:46:02 -0800

    spinner2.js implemented and tested.2

--- spinner2.js
+++ spinner2.js
@@ -4,47 +4,18 @@ const spinnerChars = "|/-\\|/-\\|/-\\|";
 
 const spinner = function (string) {
 
-let delay = 100
+  let delay = 100
 
-for (const char of string) {
+  for (const char of string) {
+    setTimeout(() => {
+      process.stdout.write(`\r${char}    `);
+    }, delay);
+    delay += 200;
+  };  
+
   setTimeout(() => {
-    process.stdout.write(`\r${char}    `);
+    process.stdout.write("\n");
   }, delay);
-  delay += 200;
-};  
-
-setTimeout(() => {
-  process.stdout.write("\n");
-}, delay);
-
-  // setTimeout(() => {
-  //   process.stdout.write("\r-   ");
-  // }, 500);
-
-  // setTimeout(() => {
-  //   process.stdout.write("\r\\   ");
-  // }, 700);
-
-  // setTimeout(() => {
-  //   process.stdout.write("\r|   ");
-  // }, 900);
-
-  // setTimeout(() => {
-  //   process.stdout.write("\r/   ");
-  // }, 1100);
-
-  // setTimeout(() => {
-  //   process.stdout.write("\r-   ");
-  // }, 1300);
-
-  // setTimeout(() => {
-  //   process.stdout.write("\r\\   ");
-  // }, 1500);
-
-  // setTimeout(() => {
-  //   process.stdout.write("\n   ");
-  // }, 1700);
-
 };
 
 spinner(spinnerChars);
